38a564f81414b63e236794c0a9c18629ae27286d
Faouzi <87236@glr.nl>
2023-02-03 22:17:23 +0100

    removed build

--- target/debug/deps/iana_time_zone-c194462dbf7ec684.d
+++
@@ -1,9 +0,0 @@
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/iana_time_zone-c194462dbf7ec684.rmeta: /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs
-
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/libiana_time_zone-c194462dbf7ec684.rlib: /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs
-
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/iana_time_zone-c194462dbf7ec684.d: /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs
-
-/Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs:
-/Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs:
-/Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs:
--- target/debug/deps/logger-98067b9a8ca6fe6b.d
+++
@@ -1,8 +0,0 @@
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/logger-98067b9a8ca6fe6b.rmeta: src/lib.rs src/logger/mod.rs src/logger/log_enum.rs src/logger/log.rs
-
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/logger-98067b9a8ca6fe6b.d: src/lib.rs src/logger/mod.rs src/logger/log_enum.rs src/logger/log.rs
-
-src/lib.rs:
-src/logger/mod.rs:
-src/logger/log_enum.rs:
-src/logger/log.rs:
