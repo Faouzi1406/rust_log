1a7c1eb71719859947eb9bfa16170abcfb03944d
Faouzi <87236@glr.nl>
2023-02-03 22:34:00 +0100

    fix

--- target/debug/deps/iana_time_zone-13edd2d35857ea1a.d
+++
@@ -1,7 +0,0 @@
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/iana_time_zone-13edd2d35857ea1a.rmeta: /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs
-
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/iana_time_zone-13edd2d35857ea1a.d: /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs /Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs
-
-/Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/lib.rs:
-/Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/ffi_utils.rs:
-/Users/faouzibouchkachekh/.cargo/registry/src/github.com-1ecc6299db9ec823/iana-time-zone-0.1.53/src/tz_macos.rs:
--- target/debug/deps/logger-2e8d597f0523b93c.d
+++
@@ -1,8 +0,0 @@
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/logger-2e8d597f0523b93c.rmeta: src/lib.rs src/logger/mod.rs src/logger/log_enum.rs src/logger/log.rs
-
-/Users/faouzibouchkachekh/Documents/GitHub/rust_log/target/debug/deps/logger-2e8d597f0523b93c.d: src/lib.rs src/logger/mod.rs src/logger/log_enum.rs src/logger/log.rs
-
-src/lib.rs:
-src/logger/mod.rs:
-src/logger/log_enum.rs:
-src/logger/log.rs:
